c38ec9afd5fae08633a3fd405ebe422182cae7c6
Jacob <jacobmatthewmurray@gmail.com>
2021-02-14 13:29:30 +0100

    frontend update

--- app/frontend/config.py
+++ app/frontend/config.py
@@ -9,17 +9,12 @@ class ProdConfig:
     """Basic Configuration."""
 
     SECRET_KEY = environ.get('SECRET_KEY')
-    
-    CAPTCHA_ENABLED = False
-
-    DATA_PATH = environ.get('DATA_PATH', '')
-    
+    DATA_PATH = environ.get('DATA_PATH', '/data')
     REDIS_URL = environ.get('REDIS_URL') 
     REDIS_QUEUE = environ.get('REDIS_QUEUE', 'vesseg')
-
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(DATA_PATH, 'db', 'vesseg.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
 
 
 class DevConfig:
@@ -27,7 +22,6 @@ class DevConfig:
     """Development Configuration."""
 
     SECRET_KEY = 'secret_key'
-    CAPTCHA_ENABLED = False
 
     DATA_PATH = '/Users/jacobmurray/projects/vesseg/data'
     
