eab4ebb1e418052b2243dae642296b2783970eba
Jacob <jacobmatthewmurray@gmail.com>
2021-02-14 23:43:25 +0100

    minor front end corrections

--- app/server/setup.sh
+++ app/server/setup.sh
@@ -17,7 +17,7 @@ apt install -y apt-transport-https ca-certificates curl software-properties-comm
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt update
-apt install -y docker-ce
+apt install -y docker-ce docker-compose
 usermod -aG docker jacob
 
 # 7. Firewall setup
