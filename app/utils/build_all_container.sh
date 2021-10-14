afa6b86ed182ed1e79a2fd92abe2cb7c0f7bf47c
Jacob <jacobmatthewmurray@gmail.com>
2021-10-14 16:01:35 +0200

    fix spelling

--- app/utils/build_all_container.sh
+++ app/utils/build_all_container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Make sure to run this script from app directory ./utils/build_all_container.sh"
-echo "Script takes arguments of the names of the containers to be build, i.e., processor."
+echo "Script takes arguments of the names of the containers to be built, i.e., processor."
 
 for container in "$@"
 do
